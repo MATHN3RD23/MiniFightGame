f9b0e1b545a72b11bb96b4c714cc5e8a8ccf37c6
MATHN3RD23 <emoolly44@gmail.com>
2025-11-29 13:28:40 -0600

    Update logic.py

--- logic.py
+++ logic.py
@@ -5,3 +5,7 @@ class Logic(QMainWindow, Ui_gameWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+    def update(self):
+        #updates the visual gui
+        pass
